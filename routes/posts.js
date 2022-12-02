d54c063259fc20c941fab7d1b51910312b08d589
Nipuna Kavishka Alahakoon <nkbakon@icloud.com>
2022-12-03 00:16:24 +0530

    Update & Delete Posts

--- routes/posts.js
+++ routes/posts.js
@@ -22,7 +22,7 @@ router.post('/post/save',(req,res)=>{
 
 //Get Posts
 
-router.get('/posts', (req,res) => {
+router.get('/posts',(req,res) => {
     Posts.find().exec((err,posts) =>{
         if(err){
             return res.status(400).json({
@@ -36,5 +36,34 @@ router.get('/posts', (req,res) => {
     });
 });
 
+//Update Posts
+
+router.put('/post/update/:id',(req,res) => {
+    Posts.findByIdAndUpdate(
+        req.params.id,
+        {
+            $set:req.body
+        },
+        (err,post)=>{
+            if(err){
+                return res.status(400).json({error:err});
+            }
+            return res.status(200).json({
+                success:"Updated successfully"
+            });
+        }
+    );
+});
+
+//Delete Posts
+
+router.delete('/post/delete/:id',(req,res) => {
+    Posts.findByIdAndRemove(req.params.id).exec((err,deletedPost) => {
+        message:"Delete unsuccessful"
+    });
+    return res.json({
+        message:"Delete successful",deletedPost
+    });
+});
 
 module.exports = router;
