9a281a50f7a72e79099700e1d6d345defbe14ac0
Nipuna Kavishka Alahakoon <nkbakon@icloud.com>
2022-12-04 00:21:33 +0530

    get a specific post (GET API)

--- routes/posts.js
+++ routes/posts.js
@@ -67,3 +67,19 @@ router.delete('/post/delete/:id',(req,res) => {
 });
 
 module.exports = router;
+
+//Get a specific post
+
+router.get('/post/:id',(req,res) => {
+    let postId = req.params.id;
+    
+    Posts.findById(postId,(err,post) => {
+        if(err){
+            return res.status(400).json({success:false, err});
+        }
+        return res.status(200).json({
+            success:true,
+            post
+        });
+    });
+});
