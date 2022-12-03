960d7743ec81c3f774d9bb035a9f4ffb143da0a3
Nipuna Kavishka Alahakoon <nkbakon@icloud.com>
2022-12-04 00:08:06 +0530

    Fetched data from MongoDB to a HTML table Used bootstrap and font awesome

--- client/src/App.js
+++ client/src/App.js
@@ -27,15 +27,39 @@ export default class App extends Component {
 
   render() { 
     return (
-      <dic>
-        {this.state.posts.map(posts => (
-          <div>
-            <p>{posts.topic}</p>
-            <p>{posts.description}</p>
-            <p>{posts.postCategory}</p>
-          </div>
-        ))}
-      </dic>
+      <div className='container'>
+        <p>All Posts</p>
+        <table class="table">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Topic</th>
+              <th scope="col">Description</th>
+              <th scope="col">Post Category</th>
+              <th scope="col">Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.posts.map((posts,index) => (
+              <tr>
+                <th scope="row">{index+1}</th>
+                <td>{posts.topic}</td>
+                <td>{posts.description}</td>
+                <td>{posts.postCategory}</td>
+                <td>
+                  <a className='btn btn-warning' href='#'>
+                    <i className='fas fa-edit'></i>&nbsp;Edit
+                  </a>
+                  &nbsp;
+                  <a className='btn btn-danger' href='#'>
+                    <i className='far fa-trash-alt'></i>&nbsp;Delete
+                  </a>
+                </td>
+              </tr>
+            ))}            
+          </tbody>
+        </table>       
+      </div>
     );
   }
 }
