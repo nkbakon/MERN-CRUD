cc46d393d695ff5104ac05c1dbd517d20725d654
Nipuna Kavishka Alahakoon <nkbakon@icloud.com>
2022-12-04 01:32:31 +0530

    Routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,65 +1,25 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from './components/Home';
+import CreatePost from './components/CreatePost';
+import EditPost from './components/EditPost';
+import PostDetails from './components/PostDetails';
+import NavBar from './components/NavBar';
 
-export default class App extends Component { 
-  constructor(props){
-    super(props);
-
-    this.state={
-      posts:[]
-    };
-  }
-
-  componentDidMount(){
-    this.retrievePosts();
-  }
-
-  retrievePosts(){
-    axios.get("http://localhost:8000/posts").then(res =>{
-      if(res.data.success){
-        this.setState({
-          posts:res.data.existingPosts
-        });
-        console.log(this.state.posts)
-      }
-    })
-  }
-
+export default class App extends Component {
   render() { 
     return (
-      <div className='container'>
-        <p>All Posts</p>
-        <table class="table">
-          <thead>
-            <tr>
-              <th scope="col">#</th>
-              <th scope="col">Topic</th>
-              <th scope="col">Description</th>
-              <th scope="col">Post Category</th>
-              <th scope="col">Action</th>
-            </tr>
-          </thead>
-          <tbody>
-            {this.state.posts.map((posts,index) => (
-              <tr>
-                <th scope="row">{index+1}</th>
-                <td>{posts.topic}</td>
-                <td>{posts.description}</td>
-                <td>{posts.postCategory}</td>
-                <td>
-                  <a className='btn btn-warning' href='#'>
-                    <i className='fas fa-edit'></i>&nbsp;Edit
-                  </a>
-                  &nbsp;
-                  <a className='btn btn-danger' href='#'>
-                    <i className='far fa-trash-alt'></i>&nbsp;Delete
-                  </a>
-                </td>
-              </tr>
-            ))}            
-          </tbody>
-        </table>       
-      </div>
-    );
+      <BrowserRouter>
+        <div className='container'>
+        <NavBar/>
+        <Routes>          
+          <Route path="/" exact component={Home}></Route>
+          <Route path="/add" component={CreatePost}></Route>
+          <Route path="/edit/:id" component={EditPost}></Route>
+          <Route path="/post/:id" component={PostDetails}></Route>
+        </Routes>         
+        </div>
+      </BrowserRouter>
+    )
   }
 }
