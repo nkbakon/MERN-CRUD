3eca6bb20d0fdf5af92561bc153a9f5dff2058c7
Nipuna Kavishka Alahakoon <nkbakon@icloud.com>
2022-12-03 22:41:46 +0530

    client side

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 
@@ -9,6 +10,7 @@ const postRoutes = require('./routes/posts');
 
 //app middleware
 app.use(bodyParser.json());
+app.use(cors());
 
 //route middleware
 app.use(postRoutes);
@@ -16,7 +18,10 @@ app.use(postRoutes);
 const PORT = 8000;
 const DB_URL = 'mongodb+srv://nipuna:nipuna1@cluster0.vk4aypw.mongodb.net/?retryWrites=true&w=majority'
 
-mongoose.connect(DB_URL)
+mongoose.connect(DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
 .then(() =>{
     console.log('DB connected');
 })
